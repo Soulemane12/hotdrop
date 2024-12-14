e26ee16174cf7720ab8afa9bbcfb1732f1f60bda
Soulemane Sow <76797044+Soulemane12@users.noreply.github.com>
2024-12-14 15:38:31 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from main import process_message
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/chat', methods=['POST'])
 def chat():
